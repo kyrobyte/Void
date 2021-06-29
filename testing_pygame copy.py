f1d505126381bb0845cacfee624a83c8320784df
Kyrobyte <67761605+kyrobyte@users.noreply.github.com>
2021-06-28 20:08:32 -0400

    Update testing_pygame copy.py

--- testing_pygame copy.py
+++ testing_pygame copy.py
@@ -5,15 +5,15 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 running = True
 pygame.display.set_caption("Creation")
-background = pygame.image.load("/Users/kunal/python/class_pygame/Bull-5-01.jpg")
-icon = pygame.image.load("/Users/kunal/python/class_pygame/neptune.png")
+background = pygame.image.load("/Users/kunal/python/class_pygame/Bull-5-01.jpg") #background
+icon = pygame.image.load("/Users/kunal/python/class_pygame/neptune.png") #The icon for the application
 pygame.display.set_icon(icon)
-myanimation = pygame.image.load("/Users/kunal/python/class_pygame/dove.png")
+myanimation = pygame.image.load("/Users/kunal/python/class_pygame/dove.png") #The first character (Use a icon for this)
 anix = 350
 aniy = 250
 playerx_change = 0
 playery_change = 0
-myanimation_2 = pygame.image.load("/Users/kunal/python/class_pygame/dove_2.png")
+myanimation_2 = pygame.image.load("/Users/kunal/python/class_pygame/dove_2.png") #The second character (Use another icon for this)
 anix_2 = random.randint(80, 400)
 aniy_2 = random.randint(80, 400)
 playerx_change_2 = 0.3
@@ -58,7 +58,7 @@ while True:
     if anix_2 >= 800:
         playerx_change_2 = -0.75
         anix_2 += playerx_change_2
-
+                                 #=====IGNORE EVERYTHING WHICH HAS BEEN COMMENTED BELOW (Commenting is aninthing that has this "#" symbol before it  )=====
             #if event.key == pygame.K_UP: #player 2
                 #playery_change_2 = -0.75
             #if event.key == pygame.K_LEFT:
